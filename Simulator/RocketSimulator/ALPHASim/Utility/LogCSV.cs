3fc04e34b58c6b7b4c15dfb15b03df3b5c7ec228
Baldstrom <jadenbottemiller@gmail.com>
2019-03-19 11:49:35 -0700

    Added ALPHASim CSV reader

--- Simulator/RocketSimulator/ALPHASim/Utility/LogCSV.cs
+++ Simulator/RocketSimulator/ALPHASim/Utility/LogCSV.cs
@@ -1,6 +1,44 @@
-﻿namespace ALPHASim.Utility
+﻿using System;
+using System.IO;
+
+namespace ALPHASim.Utility
 {
-    public static class LogCSV
+    public class LogCSV
     {
+        public const string DEFAULT_FILENAME = "ALPHASim";
+
+        private StreamWriter stream;
+
+        public LogCSV() : this(DEFAULT_FILENAME) { }
+
+        public LogCSV(string CSVFilename)
+        {
+            bool Created = false;
+            int Delimeter = 0;
+            while(!Created)
+            {
+                try
+                {
+                    FileStream fileStream;
+                    fileStream = File.Open(CSVFilename + "-" + Delimeter.ToString() + ".csv", FileMode.CreateNew);
+                    stream = new StreamWriter(fileStream);
+                    Created = true;
+                }
+                catch (IOException) { Created = false; }
+                catch (UnauthorizedAccessException)
+                {
+                    throw new UnauthorizedAccessException("Error: ALPHASim does not have file write permissions.");
+                }
+            }
+        }
+
+        public void WriteLine(string line) { stream.WriteLine(line); }
+
+        public void Write(string element, bool commaDelimit = true)
+        {
+            stream.Write(element + (commaDelimit ? "," : ""));
+        }
+
+        public void BreakLine() { stream.Write("\n"); }
     }
 }
