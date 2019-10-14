410463126420e8f15075e278d71bcd471ebb3dbe
Baldstrom <jadenbottemiller@gmail.com>
2019-10-14 15:56:25 -0700

    Added STLInsert class which will insert Rocket objects into STL files

---
+++ Simulator/RocketSimulator/RocketSimulator/STL/STLInsert.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+/// Module Name: STLInsert.cs
+/// Project: RocketSimulator
+/// Nikobotics Software
+///
+/// Contains capability to insert Rocket object into STLFile.
+/// 
+/// This source is subject to the Apache License 2.0.
+/// See https://www.apache.org/licenses/LICENSE-2.0.
+/// </summary>
+namespace RocketSimulator.STL
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Class that adds capability to convert Rocket into
+    /// an STL format. Either Binary or ASCII files.
+    /// </summary>
+    public class STLInsert
+    {
+    }
+}
